680329f3c2633443f828caa1a39754037cd89640
chberp <77995831+chberp@users.noreply.github.com>
2021-11-08 19:06:48 -0500

    Ex. 1-3

--- exercice.py
+++ exercice.py
@@ -3,20 +3,33 @@
 
 
 # TODO: Importez vos modules ici
+import math
+
 import numpy as np
 
 
 # TODO: Définissez vos fonctions ici (il en manque quelques unes)
 def linear_values() -> np.ndarray:
-    return np.array([])
+
+    return np.linspace(start=-1.3, stop=2.5, num=64)
 
 
 def coordinate_conversion(cartesian_coordinates: np.ndarray) -> np.ndarray:
-    return np.array([])
+    rayon = (cartesian_coordinates[0] ** 2 + cartesian_coordinates[1] ** 2) ** 1/2
+    angle = math.atan(cartesian_coordinates[1]/cartesian_coordinates[0])
+
+    return np.array(rayon, angle)
 
 
 def find_closest_index(values: np.ndarray, number: float) -> int:
-    return 0
+
+    return (np.abs(values - number)).argmin()
+
+
+def sine_graphic():
+    
+
+    return None
 
 
 if __name__ == '__main__':
